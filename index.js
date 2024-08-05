ecb371da2889868fb16d6984b85ec37ea5b7d18a
lakkaloko <rodrigolacombe@gmail.com>
2024-08-05 12:11:51 -0300

    Adiciona dependência googleapis ao package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ipinfo = require('ipinfo');
 const app = express();
 
 // Iniciar o servidor na porta configurada
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
