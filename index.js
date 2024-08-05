2028aa8da90bb0b60d4ac89f76fd654abddf8151
lakkaloko <rodrigolacombe@gmail.com>
2024-08-05 08:29:28 -0300

    Correção de autenticação do Google Spreadsheet e variáveis de ambiente

--- index.js
+++ index.js
@@ -1,140 +1,101 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ipinfo = require('ipinfo');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const creds = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const ipinfo = require('ipinfo');
 
 const app = express();
+const PORT = process.env.PORT || 10000;
+
 app.use(bodyParser.json());
 
-// Usando variáveis de ambiente para configurar a porta e as credenciais
-const PORT = process.env.PORT || 10000;
-const IPINFO_TOKEN = process.env.IPINFO_API_KEY;
-const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
-
-// Inicializando a planilha do Google
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
 
 async function accessSpreadsheet() {
-  await doc.useServiceAccountAuth(creds);
-  await doc.loadInfo();
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  });
+  await doc.loadInfo(); // Carrega as informações da planilha
 }
 
-accessSpreadsheet();
+accessSpreadsheet().catch(console.error);
 
-let sessions = {};
-
-// Rota para criar uma nova sessão
-app.post('/create-session', (req, res) => {
-  const { userAgent, referrer, url, screenResolution, deviceType, loadTime, browser, os } = req.body;
+app.post('/create-session', async (req, res) => {
+  const { userAgent, referrer, url } = req.body;
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  ipinfo(ip, IPINFO_TOKEN, (err, cLoc) => {
+  ipinfo(ip, async (err, cLoc) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving IP info');
-      return;
+      console.log(err);
+      res.status(500).send({ message: 'Erro ao obter geolocalização' });
+    } else {
+      const sessionData = {
+        userAgent,
+        referrer,
+        url,
+        ip,
+        city: cLoc.city || 'N/A',
+        region: cLoc.region || 'N/A',
+        country: cLoc.country || 'N/A',
+        browser: userAgent.split(' ')[userAgent.split(' ').length - 1],
+        os: userAgent.split(' ')[0],
+        sessionId: Math.random().toString(36).substr(2, 9),
+        sessionDuration: 'N/A',
+        clickCount: 0,
+        pagesVisited: [url]
+      };
+
+      const sheet = doc.sheetsByIndex[0]; // ou você pode usar doc.sheetsByTitle['Nome da aba']
+      await sheet.addRow(sessionData);
+
+      res.status(200).send({ message: 'Sessão criada.', sessionId: sessionData.sessionId });
     }
-
-    const sessionId = Math.random().toString(36).substr(2, 9);
-    const sessionData = {
-      sessionId,
-      ip,
-      userAgent,
-      referrer,
-      url,
-      screenResolution,
-      deviceType,
-      loadTime,
-      browser,
-      os,
-      city: cLoc.city || 'N/A',
-      region: cLoc.region || 'N/A',
-      country: cLoc.country || 'N/A',
-      sessionDuration: 'N/A',
-      clickCount: 0,
-      pagesVisited: [url]
-    };
-
-    sessions[sessionId] = sessionData;
-    console.log('Sessão criada e armazenada:', sessionData);
-    res.json({ message: 'Sessão criada.', sessionId });
   });
 });
 
-// Rota para capturar cliques
-app.post('/capture-click', (req, res) => {
+app.post('/capture-click', async (req, res) => {
   const { sessionId, timestamp } = req.body;
 
-  if (sessions[sessionId]) {
-    sessions[sessionId].clickCount += 1;
-    sessions[sessionId].timestamp = timestamp;
-    console.log('Clique capturado:', timestamp, 'Total de cliques:', sessions[sessionId].clickCount);
-    res.json({ message: 'Clique recebido.' });
+  const rows = await doc.sheetsByIndex[0].getRows();
+  const sessionRow = rows.find(row => row.sessionId === sessionId);
+
+  if (sessionRow) {
+    sessionRow.clickCount = parseInt(sessionRow.clickCount) + 1;
+    sessionRow.timestamp = timestamp;
+    await sessionRow.save();
+    res.status(200).send({ message: 'Clique recebido.' });
   } else {
-    res.status(404).json({ message: 'Sessão não encontrada.' });
+    res.status(404).send({ message: 'Sessão não encontrada.' });
   }
 });
 
-// Rota para capturar duração da sessão
-app.post('/session-duration', (req, res) => {
+app.post('/session-duration', async (req, res) => {
   const { sessionId, duration } = req.body;
 
-  if (sessions[sessionId]) {
-    sessions[sessionId].sessionDuration = duration;
-    console.log('Duração da sessão atualizada:', duration);
-    res.json({ message: 'Duração da sessão recebida.' });
+  const rows = await doc.sheetsByIndex[0].getRows();
+  const sessionRow = rows.find(row => row.sessionId === sessionId);
+
+  if (sessionRow) {
+    sessionRow.sessionDuration = duration;
+    await sessionRow.save();
+    res.status(200).send({ message: 'Duração da sessão recebida.' });
   } else {
-    res.status(404).json({ message: 'Sessão não encontrada.' });
+    res.status(404).send({ message: 'Sessão não encontrada.' });
   }
 });
 
-// Rota para capturar visitas de página
-app.post('/page-visit', (req, res) => {
+app.post('/page-visit', async (req, res) => {
   const { sessionId, url } = req.body;
 
-  if (sessions[sessionId]) {
-    sessions[sessionId].pagesVisited.push(url);
-    console.log('Página visitada:', url);
-    res.json({ message: 'Página visitada recebida.' });
+  const rows = await doc.sheetsByIndex[0].getRows();
+  const sessionRow = rows.find(row => row.sessionId === sessionId);
+
+  if (sessionRow) {
+    sessionRow.pagesVisited += `, ${url}`;
+    await sessionRow.save();
+    res.status(200).send({ message: 'Página visitada recebida.' });
   } else {
-    res.status(404).json({ message: 'Sessão não encontrada.' });
-  }
-});
-
-// Função para enviar dados para o Google Sheets
-async function sendToGoogleSheets(sessionData) {
-  const sheet = doc.sheetsByIndex[0];
-  await sheet.addRow({
-    'IP Address': sessionData.ip,
-    'User Agent': sessionData.userAgent,
-    'Browser': sessionData.browser,
-    'Operating System': sessionData.os,
-    'Referrer': sessionData.referrer,
-    'URL': sessionData.url,
-    'Timestamp': sessionData.timestamp,
-    'Screen Resolution': sessionData.screenResolution,
-    'Device Type': sessionData.deviceType,
-    'City': sessionData.city,
-    'Region': sessionData.region,
-    'Country': sessionData.country,
-    'Load Time': sessionData.loadTime,
-    'Session Duration': sessionData.sessionDuration,
-    'Click Count': sessionData.clickCount
-  });
-  console.log('Dados enviados para o Google Sheets:', sessionData);
-}
-
-// Enviar dados de sessão para o Google Sheets ao encerrar a sessão
-app.post('/end-session', (req, res) => {
-  const { sessionId } = req.body;
-
-  if (sessions[sessionId]) {
-    sendToGoogleSheets(sessions[sessionId]);
-    delete sessions[sessionId];
-    res.json({ message: 'Sessão encerrada e dados enviados para o Google Sheets.' });
-  } else {
-    res.status(404).json({ message: 'Sessão não encontrada.' });
+    res.status(404).send({ message: 'Sessão não encontrada.' });
   }
 });
 
